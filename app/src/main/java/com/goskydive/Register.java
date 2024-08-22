cdcd9e29e70cf769c11901f85f364c583cc7f2d7
kuba88pl <jakub.kulawik@gmail.com>
2024-08-22 14:07:36 +0200

    Register activity works fine, storing user email and password in firebase propetly (without username)

--- app/src/main/java/com/goskydive/Register.java
+++ app/src/main/java/com/goskydive/Register.java
@@ -1,19 +1,32 @@
 package com.goskydive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class Register extends AppCompatActivity {
 
-//    EditText username, email, password;
-//    ImageButton signup_button;
+    EditText username, email, password;
+    ImageButton signup_button;
+    TextView alreadyRegistered;
+    FirebaseAuth fAuth;
 
 
     @Override
@@ -25,6 +38,56 @@ public class Register extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        username = findViewById(R.id.username);
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        signup_button = findViewById(R.id.googlesignupbutton);
+        alreadyRegistered = findViewById(R.id.allready_registred_text);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        //Checking is user sign in
+
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }
+
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailtext = email.getText().toString().trim();
+                String passwordText = password.getText().toString().trim();
+
+                //email and password validation
+
+                if (TextUtils.isEmpty(emailtext)) {
+                    email.setError("Email is reguired.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(passwordText) || passwordText.length() < 6) {
+                    password.setError("Password should contains min. 6 or more characters long!");
+                    return;
+                }
+
+                // user registration in firebase
+
+                fAuth.createUserWithEmailAndPassword(emailtext, passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Register.this, "User created!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        } else {
+                            Toast.makeText(Register.this, "Error! Try again!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+            }
         });
     }
 }
