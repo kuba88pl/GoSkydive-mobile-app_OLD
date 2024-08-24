ec64bb29eefe86794d7acc9732dbac891a8ef0dd
kuba88pl <jakub.kulawik@gmail.com>
2024-08-24 19:16:03 +0200

    Created UserProfile activity and implemented retriving user data - wokrs fine!

--- app/src/main/java/com/goskydive/MainActivity.java
+++ app/src/main/java/com/goskydive/MainActivity.java
@@ -1,6 +1,9 @@
 package com.goskydive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageButton personLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +24,14 @@ public class MainActivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+        personLink = findViewById(R.id.person_link_button);
+
+        personLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), UserProfile.class));
+            }
+        });
 
 
     }
---
+++ app/src/main/java/com/goskydive/UserProfile.java
@@ -0,0 +1,67 @@
+package com.goskydive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+public class UserProfile extends AppCompatActivity {
+
+    ImageButton logoutButton;
+    TextView name, email;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userId;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_user_profile);
+
+        name = findViewById(R.id.your_name_text);
+        email = findViewById(R.id.your_email_text);
+
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+
+        userId = fAuth.getCurrentUser().getUid();
+
+//ratriving data from firestore
+
+        DocumentReference documentReference = fStore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                name.setText(documentSnapshot.getString("userName"));
+                email.setText(documentSnapshot.getString("email"));
+
+            }
+        });
+
+
+    }
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getApplicationContext(), Login.class));
+        finish();
+    }
+
+}
