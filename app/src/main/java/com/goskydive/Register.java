b3219589cd8e21cd53e97957db364115e3f132fe
kuba88pl <jakub.kulawik@gmail.com>
2024-08-24 16:05:53 +0200

    Storing to the firestore doesnt work, app crush

--- app/src/main/java/com/goskydive/Register.java
+++ app/src/main/java/com/goskydive/Register.java
@@ -3,6 +3,7 @@ package com.goskydive;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -17,9 +18,15 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
@@ -27,6 +34,8 @@ public class Register extends AppCompatActivity {
     ImageButton signUpButton;
     TextView alreadyRegistered;
     FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    String userID;
 
 
     @Override
@@ -47,6 +56,7 @@ public class Register extends AppCompatActivity {
         alreadyRegistered = findViewById(R.id.allready_registred_text);
 
         fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
         //Checking is user sign in
 
@@ -60,6 +70,7 @@ public class Register extends AppCompatActivity {
             public void onClick(View view) {
                 String emailtext = email.getText().toString().trim();
                 String passwordText = password.getText().toString().trim();
+                String usernameText = username.getText().toString();
 
                 //email and password validation
 
@@ -75,25 +86,38 @@ public class Register extends AppCompatActivity {
 
                 // user registration in firebase
 
-                fAuth.createUserWithEmailAndPassword(emailtext, passwordText).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(Register.this, "User created!", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        } else {
-                            Toast.makeText(Register.this, "Error! Try again!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                        }
+                fAuth.createUserWithEmailAndPassword(emailtext, passwordText).addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(Register.this, "User created!", Toast.LENGTH_SHORT).show();
+
+                        //storing user data to firestore
+                        userID = fAuth.getCurrentUser().getUid();
+                        DocumentReference documentReference = fStore.collection("users").document(userID);
+                        Map<String, Object> user = new HashMap<>();
+                        user.put("userName", username);
+                        user.put("email", email);
+
+                        documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                            public static final String TAG = "TAG";
+
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "onSucces user profile is created for " + userID);
+
+                            }
+                        });
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    } else {
+                        Toast.makeText(Register.this, "Error! Try again!" + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
             }
         });
 
-        alreadyRegistered.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Login.class));
-            }
+        alreadyRegistered.setOnClickListener(view ->
+        {
+            startActivity(new Intent(getApplicationContext(), Login.class));
         });
     }
 }
