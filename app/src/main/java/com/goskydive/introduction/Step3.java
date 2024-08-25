5fc180e924a2dab14bf7b5b2366b2eaee6135464
kuba88pl <jakub.kulawik@gmail.com>
2024-08-25 17:19:21 +0200

    introduction package xml was created

--- app/src/main/java/com/goskydive/introduction/Step3.java
+++ app/src/main/java/com/goskydive/introduction/Step3.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -15,35 +14,31 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.goskydive.R;
 
-public class Step2 extends AppCompatActivity {
+public class Step3 extends AppCompatActivity {
 
-    EditText howManyJumps;
     Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_step2);
+        setContentView(R.layout.activity_step3);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
 
-        howManyJumps = findViewById(R.id.how_many_jumps_edit_text);
         nextButton = findViewById(R.id.next_button);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(howManyJumps.getText().toString().isEmpty()) {
-                    Toast.makeText(Step2.this, "Type number of jumps", Toast.LENGTH_SHORT).show();
-                } else {
-                    startActivity(new Intent(getApplicationContext(), Step3.class));
-                }
+                    startActivity(new Intent(getApplicationContext(), Step4.class));
+
             }
         });
+
     }
 }
--- app/src/main/java/com/goskydive/introduction/Step6.java
+++ app/src/main/java/com/goskydive/introduction/Step6.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,34 +13,29 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.goskydive.R;
 
-public class Step2 extends AppCompatActivity {
+public class Step6 extends AppCompatActivity {
 
-    EditText howManyJumps;
     Button nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_step2);
+        setContentView(R.layout.activity_step6);
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
 //            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
 
-        howManyJumps = findViewById(R.id.how_many_jumps_edit_text);
         nextButton = findViewById(R.id.next_button);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                if(howManyJumps.getText().toString().isEmpty()) {
-                    Toast.makeText(Step2.this, "Type number of jumps", Toast.LENGTH_SHORT).show();
-                } else {
-                    startActivity(new Intent(getApplicationContext(), Step3.class));
-                }
+                startActivity(new Intent(getApplicationContext(), Step5.class));
+
             }
         });
     }
