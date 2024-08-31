9f0fae62550dcbbf22da53f6fa96572ad73289ac
Kuba <jakub.kulawik@gmail.com>
2024-08-31 22:51:47 +0200

    AddJumpActivity - progress in seekbar works fine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
+    implementation(libs.ui.graphics.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/goskydive/logbook/AddJumpActivity.java
+++ app/src/main/java/com/goskydive/logbook/AddJumpActivity.java
@@ -1,10 +1,8 @@
 package com.goskydive.logbook;
 
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -20,7 +18,6 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 
-
 import com.goskydive.R;
 
 import java.text.SimpleDateFormat;
@@ -29,12 +26,9 @@ import java.util.Date;
 
 public class AddJumpActivity extends AppCompatActivity {
 
-    public static final String TAG = "TAG";
-    public static final String TAG1 = "TAG";
-    TextView jumpNumber, editTextDate;
+    TextView jumpNumber, editTextDate, seekBarResult;
     Spinner chooseJumpType, setPlane;
-    ListView jumpStyleListView;
-    SeekBar heigh;
+    SeekBar highSeekBar;
 
 
     FirebaseAuth fAuth;
@@ -42,10 +36,13 @@ public class AddJumpActivity extends AppCompatActivity {
     String userId;
 
     long nextJumpPlusOne;
+    long userJumpValue;
 
     Date date = new Date();
     SimpleDateFormat todayDate = new SimpleDateFormat("dd.MM.yyyy");
     String todayDateToString = todayDate.format(date);
+
+    int[] seekBarHighValues = {800, 1000, 1200, 1500, 2000, 2500, 3000, 3500, 4000, 4500};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,9 +52,11 @@ public class AddJumpActivity extends AppCompatActivity {
 
         jumpNumber = findViewById(R.id.jump_number_number);
         editTextDate = findViewById(R.id.editTextDate);
-        heigh = findViewById(R.id.heigh_seekbar);
+        highSeekBar = findViewById(R.id.heigh_seekbar);
         chooseJumpType = findViewById(R.id.spinner_jump_type);
         setPlane = findViewById(R.id.set_plane_spinner);
+        seekBarResult = findViewById(R.id.seekbar_result);
+
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
@@ -80,7 +79,7 @@ public class AddJumpActivity extends AppCompatActivity {
             }
         });
 
-        //spinner jumpType
+        //spinner jumpType from firestore
 
         fStore.collection("jumpStyles").get().addOnSuccessListener(result -> {
             ArrayList<String> items = new ArrayList<>();
@@ -110,5 +109,40 @@ public class AddJumpActivity extends AppCompatActivity {
 
         });
 
+        //seekbar high
+
+        GradientDrawable gradientDrawable = new GradientDrawable(
+                GradientDrawable.Orientation.LEFT_RIGHT,
+                new int[]{R.color.red_seek_bar, R.color.light_red_seek_bar, R.color.yellow_seek_bar, R.color.light_yellow_seek_bar,
+                        R.color.light_green_seek_bar, R.color.green_seek_bar,
+                });
+        gradientDrawable.setCornerRadius(10);
+        highSeekBar.setProgressDrawable(gradientDrawable);
+
+
+        highSeekBar.setMax(4500);
+        highSeekBar.setMin(800);
+        highSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                seekBarResult.setText(progress + " meters AGL");
+                //progress trzeba zapisac do drugiej zmiennej ktora bedzie przekazana do bazy
+                userJumpValue = Long.valueOf(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+
+        });
+
+
     }
 }
+
