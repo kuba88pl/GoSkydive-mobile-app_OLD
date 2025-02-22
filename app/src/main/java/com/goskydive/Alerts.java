1c19869d8147d893f0abaf3a99d98bc165e64968
Kuba <jakub.kulawik@gmail.com>
2025-02-22 12:04:12 +0100

    Added menu drawer in all activities

--- app/src/main/java/com/goskydive/Alerts.java
+++ app/src/main/java/com/goskydive/Alerts.java
@@ -1,14 +1,28 @@
 package com.goskydive;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.goskydive.logbook.LogBook;
 
 public class Alerts extends AppCompatActivity {
+
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +30,57 @@ public class Alerts extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_alerts);
 
+        toolbar = findViewById(R.id.toolbar);
+        navigationView = findViewById(R.id.navigation_view);
+        drawerLayout = findViewById(R.id.drawerLayout);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(Alerts.this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.black));
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.nav_logbok) {
+                    Intent intent = new Intent(getApplicationContext(), LogBook.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_messages) {
+                    Intent intent = new Intent(getApplicationContext(), Messages.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_friends) {
+                    Intent intent = new Intent(getApplicationContext(), Friends.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_weather) {
+                    Intent intent = new Intent(getApplicationContext(), Weather.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_alerts) {
+                    Intent intent = new Intent(getApplicationContext(), Alerts.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_settings) {
+                    Intent intent = new Intent(getApplicationContext(), Settings.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_logbok) {
+                    Intent intent = new Intent(getApplicationContext(), LogBook.class);
+                    startActivity(intent);
+                }
+                if (item.getItemId() == R.id.nav_logout) {
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    FirebaseAuth.getInstance().signOut();
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+
+
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/goskydive/UserProfile.java
+++ app/src/main/java/com/goskydive/UserProfile.java
@@ -6,29 +6,41 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.goskydive.logbook.LogBook;
 
 public class UserProfile extends AppCompatActivity {
 
-
+    Toolbar toolbar;
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
 
     Button logoutButton, changeProfileImageButton;
     TextView name, email, totalJumps, textview;
@@ -36,6 +48,8 @@ public class UserProfile extends AppCompatActivity {
 
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
+    //TODO
+    // StorageReference storageReference;
     String userId;
 
     @Override
@@ -43,6 +57,59 @@ public class UserProfile extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_user_profile);
+
+            toolbar = findViewById(R.id.toolbar);
+            navigationView = findViewById(R.id.navigation_view);
+            drawerLayout = findViewById(R.id.drawerLayout);
+
+
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(UserProfile.this, drawerLayout, toolbar, R.string.open, R.string.close);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.getDrawerArrowDrawable().setColor(getResources().getColor(R.color.black));
+            toggle.syncState();
+
+            navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    if (item.getItemId() == R.id.nav_logbok) {
+                        Intent intent = new Intent(getApplicationContext(), LogBook.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_messages) {
+                        Intent intent = new Intent(getApplicationContext(), Messages.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_friends) {
+                        Intent intent = new Intent(getApplicationContext(), Friends.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_weather) {
+                        Intent intent = new Intent(getApplicationContext(), Weather.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_alerts) {
+                        Intent intent = new Intent(getApplicationContext(), Alerts.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_settings) {
+                        Intent intent = new Intent(getApplicationContext(), Settings.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_logbok) {
+                        Intent intent = new Intent(getApplicationContext(), LogBook.class);
+                        startActivity(intent);
+                    }
+                    if (item.getItemId() == R.id.nav_logout) {
+                        Intent intent = new Intent(getApplicationContext(), Login.class);
+                        FirebaseAuth.getInstance().signOut();
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    }
+
+
+                    return true;
+                }
+            });
 
         name = findViewById(R.id.your_name_text);
         email = findViewById(R.id.your_email_text);
@@ -55,6 +122,8 @@ public class UserProfile extends AppCompatActivity {
 
         fAuth = FirebaseAuth.getInstance();
         fStore = FirebaseFirestore.getInstance();
+        //TODO
+        // storageReference = FirebaseStorage.getInstance().getReference();
 
         userId = fAuth.getCurrentUser().getUid();
 
@@ -77,6 +146,8 @@ public class UserProfile extends AppCompatActivity {
                     long jumpNo = jumpNumberReferenceSnapshot.getLong("allJumps");
                     textview.setText(String.valueOf(jumpNo));
                 } catch (NullPointerException e) {
+                   //TODO
+                    //add toast message, that user has not made any jumps yet
                 }
 
             }
@@ -85,6 +156,7 @@ public class UserProfile extends AppCompatActivity {
         logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //TODO
 //                FirebaseAuth.getInstance().signOut();
 //                startActivity(new Intent(getApplicationContext(), Login.class));
 //                finish();
@@ -110,12 +182,32 @@ public class UserProfile extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == 1000) {
-            if(resultCode == Activity.RESULT_OK) {
+            if (resultCode == Activity.RESULT_OK) {
                 Uri imageUri = data.getData();
                 profileImage.setImageURI(imageUri);
+//TODO
+//                uploadImageToFirebase(imageUri);
+
             }
         }
     }
+//TODO
+//    private void uploadImageToFirebase(Uri imageUri) {
+//      upload profile image to firebase storage
+//
+//        StorageReference fileReference = storageReference.child("profile.jpg");
+//        fileReference.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//            @Override
+//            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                Toast.makeText(UserProfile.this, "Profile image uploaded", Toast.LENGTH_SHORT).show();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(UserProfile.this, "Error! Profile image not uploaded", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//    }
 
     public void logout(View view) {
         FirebaseAuth.getInstance().signOut();
